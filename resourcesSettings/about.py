44e57fad232aa449c837e9775a45e39ffb0d8304
EdwinRodger <92982808+EdwinRodger@users.noreply.github.com>
2022-05-08 17:53:22 +0530

    v1.2

---
+++ resourcesSettings/about.py
@@ -0,0 +1,5 @@
+class about():
+    def __init__(self):
+        print("\n" * 5)
+        print("Hi! I am Edwin Rodger, 17 years old.\nI always wanted to make a application,\nas I am learning about python in school\nI thought to make a simple pyhton\napplication. Now what is more good than\na total offline diary\journal which runs\non command prompt and has a pretty good UI\n\nThis is my first ever project so there are\ntoo many mistakes and I apologize for that.\nBesides mistakes, feel free to review the\ncode and run the apllication.\n\nFirst release - 15/04/2022")
+        wait = input("Press enter to continue")
