96d15e573b6df696b11f1d1f395ce7bd54e4d02e
EdwinRodger <vaidiktest@gmail.com>
2022-05-23 16:01:51 +0530

    v3.0

--- CMD-Diary.py
+++ CMD-Diary.py
@@ -1,3 +1,11 @@
+import os.path
+
+# newpath command to create a CMD-Diary folder in Programfiles if not already exists
+
+newpath = 'C:\\CMD-Diary\\'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
 if __name__=="__main__":
     from res import DiaryEntry
     from res import UserInterface
--- res/UserInterface.py
+++ res/UserInterface.py
@@ -1,12 +1,48 @@
 # Python Modules Used
-
-from res import DiaryEntry as ds
-from res import ShowSettings as ss
+from tkinter import *
+from res import DiaryEntry as ds, ShowSettings as ss, OnlineHelp as oh
+from res.GUI import today_entry as te, another_entry as ae, edit_entry as ee, view_entry as ve
 
 # Clear command to make a five line space to lessen the congestion
 
 def clear():
     print("\n"*5)
+
+# Online Help UI for CMD-Diary
+
+def onlinehelpUI():
+    while True:
+        clear()
+        print("O n l i n e H e l p")
+        print("-"*19)
+        print("1. Website")
+        print("2. Changelog")
+        print("3. Code of Conduct")
+        print("4. Contributing")
+        print("5. License")
+        print("6. Releases")
+        print("7. Back to settings")
+        try:
+            choice = int(input("Enter Your Choice (1 - 7): "))
+            if choice==1:
+                oh.website()
+            elif choice==2:
+                oh.changelog()
+            elif choice==3:
+                oh.code_of_conduct()
+            elif choice==4:
+                oh.contributing()
+            elif choice==5:
+                oh.license()
+            elif choice==6:
+                oh.releases()
+            elif choice==7:
+                break
+            else:
+                wait = input("Please Choose From 1 to 7 Only")
+        except:
+            print("\n\nInvalid choice")
+            wait = input()
 
 # Settings UI for CMD-Diary
 
@@ -20,9 +56,10 @@ def settingsUI():
         print("3. Contribution")
         print("4. Help")
         print("5. Diary Template")
-        print("6. Back To Main Menu")
+        print("6. Online Help")
+        print("7. Back To Main Menu")
         try:
-            choice = int(input("Enter Your Choice (1 - 5): "))
+            choice = int(input("Enter Your Choice (1 - 7): "))
             if choice==1:
                 ds.entry_path()
             elif choice==2:
@@ -31,12 +68,14 @@ def settingsUI():
                 ss.contribution()
             elif choice==4:
                 ss.help()
-            elif choice == 5:
+            elif choice==5:
                 ss.template()
             elif choice==6:
+                onlinehelpUI()
+            elif choice==7:
                 break
             else:
-                print("Please Choose From 1 to 5 Only")
+                print("Please Choose From 1 to 7 Only")
                 wait = input()
         except:
             print("\n\nInvalid choice")
@@ -46,30 +85,34 @@ def settingsUI():
 
 def main_menuUI():
     while True:
-        clear()
-        print("   P y D i a r y")
-        print("-"*19)
-        print("1. Add Today's Entry")
-        print("2. Add Other Day Entry")
-        print("3. Add To Entry")
-        print("4. View An Entry")
-        print("5. Settings")
-        print("6. Exit")
-        choice = int(input("\n\nEnter Your Choice (1 - 6): "))
-        if choice==1:
-            ds.diary_template()
-        elif choice==2:
-            ds.other_entry()
-        elif choice==3:
-            ds.edit_entry()
-        elif choice==4:
-            ds.view_entry()
-        elif choice==5:
-            settingsUI()
-        elif choice==6:
-            break
-        else:
-            print("Please choose from 1 to 6 Only")
+        try:
+            clear()
+            print("   P y D i a r y")
+            print("-"*19)
+            print("1. Add Today's Entry")
+            print("2. Add Other Day Entry")
+            print("3. Add To Entry")
+            print("4. View An Entry")
+            print("5. Settings")
+            print("6. Exit")
+            choice = int(input("\n\nEnter Your Choice (1 - 6): "))
+            if choice==1:
+                te.today_entry()
+            elif choice==2:
+                ae.another_entry()
+            elif choice==3:
+                ee.edit_entry()
+            elif choice==4:
+                ve.view_entry()
+            elif choice==5:
+                settingsUI()
+            elif choice==6:
+                break
+            else:
+                print("Please choose from 1 to 6 Only")
+                wait = input()
+        except:
+            print("\n\nInvalid choice")
             wait = input()
 
 
