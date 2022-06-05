bddab527532ceca26a3bbc3543348a8140c13ee7
EdwinRodger <vaidiktest@gmail.com>
2022-06-05 12:55:48 +0530

    added fuction to create pydiaries folder in user directory

--- PyDiaries/PyDiaries.py
+++ PyDiaries/PyDiaries.py
@@ -1,4 +1,16 @@
 from gui.MainUI import main
+from os.path import expanduser, os
+
+# Command to create a CMD-Diary folder in User directory if not already exists
+home = expanduser("~")
+newpath = f"{home}\\CMD-Diary\\"
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+if not os.path.exists(f"{newpath}\\Textfiles\\"):
+    os.makedirs(f"{newpath}\\Textfiles\\")
+with open(f"{home}\\CMD-Diary\\Textfiles\\path.txt", "w") as homedir:
+    homedir.write(newpath)
+
 
 if __name__=="__main__":
     main()
--- PyDiaries/gui/CalendarUI.py
+++ PyDiaries/gui/CalendarUI.py
@@ -1,6 +1,4 @@
 import tkcalendar as tkc
-import customtkinter as ctk
-import tkinter as tk
 
 def calendar(master):
     cal = tkc.Calendar(master, font="Comic_Sans 21", showweeknumbers=False)
