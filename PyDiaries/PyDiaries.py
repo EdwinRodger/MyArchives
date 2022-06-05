105dea6a9c53a3a8b209d1efca3d5f3d325718d3
EdwinRodger <vaidiktest@gmail.com>
2022-06-05 13:00:02 +0530

    fixed folder name

--- PyDiaries/PyDiaries.py
+++ PyDiaries/PyDiaries.py
@@ -3,12 +3,12 @@ from os.path import expanduser, os
 
 # Command to create a CMD-Diary folder in User directory if not already exists
 home = expanduser("~")
-newpath = f"{home}\\CMD-Diary\\"
+newpath = f"{home}\\PyDiaries\\"
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 if not os.path.exists(f"{newpath}\\Textfiles\\"):
     os.makedirs(f"{newpath}\\Textfiles\\")
-with open(f"{home}\\CMD-Diary\\Textfiles\\path.txt", "w") as homedir:
+with open(f"{home}\\PyDiaries\\Textfiles\\path.txt", "w") as homedir:
     homedir.write(newpath)
 
 
