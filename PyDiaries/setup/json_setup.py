c94016f714a22baef21257986c29b5bb8ca6ca85
EdwinRodger <vaidiktest@gmail.com>
2022-06-07 18:43:40 +0530

    updated json_setup.py to work only once

--- PyDiaries/setup/json_setup.py
+++ PyDiaries/setup/json_setup.py
@@ -6,6 +6,15 @@ import datetime
 date = datetime.datetime.today()
 _, path = home_directory()
 
+try:
+    with open(f"{path}Textfiles\json.txt", "r") as f:
+        condition = int(f.read())
+except:
+    with open(f"{path}Textfiles\json.txt", "w") as f:
+        f.write("0")
+    with open(f"{path}Textfiles\json.txt", "r") as f:
+        condition = int(f.read())
+
 contents = {
     "metadata":{
         "application":"PyDiaries",
@@ -17,5 +26,10 @@ contents = {
 
 
 def setup_json():
-    with open(f"{path}\\DiaryEntry.json", "w") as f:
-        json.dump(contents, f, indent=4)
+    if condition==0:
+        with open(f"{path}\\DiaryEntry.json", "w") as f:
+            json.dump(contents, f, indent=4)
+        with open(f"{path}Textfiles\json.txt", "w") as f:
+            f.write("1")
+    elif condition==1:
+        pass
