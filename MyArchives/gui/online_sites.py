1f859f3161945bd6801aa784e8d5a753c7ca5988
EdwinRodger <vaidiktest@gmail.com>
2022-06-19 15:24:51 +0530

    changed site URLs

--- MyArchives/gui/online_sites.py
+++ MyArchives/gui/online_sites.py
@@ -3,29 +3,29 @@ from webbrowser import open_new_tab
 
 def changelog():
     open_new_tab(
-        "https://github.com/EdwinRodger/CMD-Diary/blob/main/.github/CHANGELOG.md"
+        "https://github.com/EdwinRodger/MyArchives/blob/main/.github/CHANGELOG.md"
     )
 
 
 def code_of_conduct():
     open_new_tab(
-        "https://github.com/EdwinRodger/CMD-Diary/blob/main/.github/CODE_OF_CONDUCT.md"
+        "https://github.com/EdwinRodger/MyArchives/blob/main/.github/CODE_OF_CONDUCT.md"
     )
 
 
 def contributing():
     open_new_tab(
-        "https://github.com/EdwinRodger/CMD-Diary/blob/main/.github/CONTRIBUTING.md"
+        "https://github.com/EdwinRodger/MyArchives/blob/main/.github/CONTRIBUTING.md"
     )
 
 
 def license():
-    open_new_tab("https://github.com/EdwinRodger/CMD-Diary/blob/main/LICENSE")
+    open_new_tab("https://github.com/EdwinRodger/MyArchives/blob/main/LICENSE")
 
 
 def releases():
-    open_new_tab("https://github.com/EdwinRodger/CMD-Diary/releases")
+    open_new_tab("https://github.com/EdwinRodger/MyArchives/releases")
 
 
 def website():  #
-    open_new_tab("https://github.com/EdwinRodger/CMD-Diary")
+    open_new_tab("https://github.com/EdwinRodger/MyArchives")
