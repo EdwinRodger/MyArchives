b0c14312b99c5c033e0d2907168c29b40e4c0a5f
EdwinRodger <vaidiktest@gmail.com>
2022-06-23 18:05:45 +0530

    removed files_setup.py

--- MyArchives/setup/main_setup.py
+++ MyArchives/setup/main_setup.py
@@ -1,4 +1,3 @@
-from setup.files_setup import file_setup
 from setup.folders_setup import folder_setup
 
 from .image_setup import icon_download
@@ -6,8 +5,6 @@ from .update_setup import update
 
 
 def setup():
-    folder_setup()
-    file_setup.md_setup()
-    file_setup.pathtxt_setup()
-    icon_download()
     update()
+    folder_setup()
+    icon_download()
