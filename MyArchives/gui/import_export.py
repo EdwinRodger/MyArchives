82a366eb67ec9a4886d0d454bba3a5a08956a7c0
EdwinRodger <vaidiktest@gmail.com>
2022-06-25 19:30:47 +0530

    updated import_export.py - added title in filedialogs - filepath mustexist=True - added .zip file in file types when selecting import zip - Changed export file name

--- MyArchives/gui/import_export.py
+++ MyArchives/gui/import_export.py
@@ -1,29 +1,29 @@
 from shutil import make_archive
 import zipfile
-from tkinter.filedialog import askdirectory, askopenfile
-from tkinter import filedialog
+from tkinter.filedialog import askdirectory, askopenfilename
 from .home_dir import home_directory
 from os import listdir
 
 homepath = home_directory()
 
 def export_zip():
-    save_dir = askdirectory()
-    make_archive(f"{save_dir}/MyArchives", 'zip', f"{homepath}MyArchive")
+    save_dir = askdirectory(title="Select folder to save zip file", mustexist=True)
+    make_archive(f"{save_dir}/MyArchives-Export", 'zip', f"{homepath}MyArchive")
 
 def import_zip():
-    zip_dir = filedialog.askopenfilename()
+    zip_dir = askopenfilename(title="Select A .zip File To Import", filetypes=(("zip files", "*.zip"),("all files", "*.*")))
     # print(zip_dir)
     with zipfile.ZipFile(zip_dir, 'r') as zip_ref:
         zip_ref.extractall(f"{homepath}/MyArchive")
 
 def export_txt():
-    save_dir = askdirectory()
+    save_dir = askdirectory(title="Select folder to save text file", mustexist=True)
     files = []
+    
     for i in listdir(f"{homepath}MyArchive/"):
         files.append(i)
 
-    with open(f'{save_dir}/export.txt', 'w') as outfile:
+    with open(f'{save_dir}/MyArchives-Export.txt', 'w') as outfile:
         outfile.write("MyArchives\n\n\n\n")
         for fname in files:
             with open(f'{homepath}MyArchive/{fname}') as infile:
