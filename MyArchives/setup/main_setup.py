406190ae363de09286f091b9082a45d4bd424056
EdwinRodger <vaidiktest@gmail.com>
2022-07-06 17:11:19 +0530

    removed "Change Font Settings" for now - when changing fonts, the whole UI was getting messed up so I have postponed it's development for future date

--- MyArchives/setup/main_setup.py
+++ MyArchives/setup/main_setup.py
@@ -1,11 +1,9 @@
 from .folders_setup import folder_setup
 from .image_setup import icon_download
 from .update_setup import update
-from .file_setup import font_file
 
 
 def setup():
     folder_setup()
     update()
     icon_download()
-    font_file()
