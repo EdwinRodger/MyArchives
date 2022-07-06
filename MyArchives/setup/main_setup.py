669a0fe750386d02d6f7208bcd1bab4b99db6ccc
EdwinRodger <vaidiktest@gmail.com>
2022-07-06 16:26:21 +0530

    added file_setup.py for font settings

--- MyArchives/setup/main_setup.py
+++ MyArchives/setup/main_setup.py
@@ -1,9 +1,11 @@
 from .folders_setup import folder_setup
 from .image_setup import icon_download
 from .update_setup import update
+from .file_setup import font_file
 
 
 def setup():
     folder_setup()
     update()
     icon_download()
+    font_file()
