79164353510fa97baffe38f4315b2d609e559ee5
EdwinRodger <vaidiktest@gmail.com>
2022-07-20 15:45:06 +0530

    created separate file for menubar functions

--- MyArchives/gui/main_gui.py
+++ MyArchives/gui/main_gui.py
@@ -1,32 +1,23 @@
 # Python Libraries
-import threading
 import tkinter as tk
-from os import remove
 from sys import exit
-from tkinter.messagebox import askyesno, showerror
 from tkinter.scrolledtext import ScrolledText
 
 # Third Party Libraries
 import customtkinter as ctk
-from pyttsx3 import init
-from sounddevice import rec, wait
-from speech_recognition import AudioFile, Recognizer
 from tkcalendar import Calendar
-from wavio import write
-
 
 # MyArchives Libraries
 from .encrypt_decrypt import encrypt
 from .home_dir import home_directory
 from .import_export import *
+from .menubar_functions import *
 from .online_sites import *
 from .password import change_password
 from .themes import *
 
 homepath = home_directory()
 
-# pyttsx3.init()
-engine = init()
 
 # This is Pyinstaller special import and doesn't come with python and can't be downloaded with package manager.
 try:
@@ -121,169 +112,13 @@ def main():
     my_menu = tk.Menu(window)
     window.config(menu=my_menu)
 
-    # Cut Text
-    def cut_text(e):
-        global selected
-        # Check to see if keyboard shortcut used
-        if e:
-            selected = window.clipboard_get()
-        else:
-            if text_box.selection_get():
-                # Grab selected text from text box
-                selected = text_box.selection_get()
-
-                # Delete Selected Text from text box
-                text_box.delete("sel.first", "sel.last")
-
-                # Clear the clipboard then apptk.end
-                window.clipboard_clear()
-                window.clipboard_apptk.end(selected)
-
-    # Copy Text
-    def copy_text(e):
-        global selected
-        # check to see if we used keyboard shortcuts
-        if e:
-            selected = window.clipboard_get()
-
-        if text_box.selection_get():
-            # Grab selected text from text box
-            selected = text_box.selection_get()
-
-            # Clear the clipboard then apptk.end
-            window.clipboard_clear()
-            window.clipboard_apptk.end(selected)
-
-    # Paste Text
-    def paste_text(e):
-        global selected
-        # Check to see if keyboard shortcut used
-        if e:
-            selected = window.clipboard_get()
-        else:
-            if selected:
-                position = text_box.index(tk.INSERT)
-                text_box.insert(position, selected)
-
-    # Select all Text
-    def select_all(e):
-        # Add sel tag to select all text
-        text_box.tag_add("sel", "1.0", "tk.end")
-
-    # Clear All Text
-    def clear_all():
-        text_box.delete(1.0, tk.END)
-
-    # Text-To-Speech (threading)
-    def ttst():
-        def tts():
-            voices = engine.getProperty("voices")
-            with open(f"{homepath}Textfiles/voices.txt", "r") as f:
-                voice = f.read()
-            if voice == "0":
-                engine.setProperty(
-                    "voice", voices[0].id
-                )  # changing index, changes voices. 0 for male
-                engine.say(text_box.get(0.0, tk.END))
-                engine.runAndWait()
-            elif voice == "1":
-                engine.setProperty(
-                    "voice", voices[1].id
-                )  # changing index, changes voices. 1 for female
-                engine.say(text_box.get(0.0, tk.END))
-                engine.runAndWait()
-            else:
-                engine.say(
-                    "There is some problem with Text To Speech, Try to change narrators voice and try again"
-                )
-                engine.runAndWait()
-
-        tts_threading = threading.Thread(target=tts)
-        tts_threading.start()
-
-    # Speech-To-Text (threading)
-    def sttt():
-        # tkinter.messagebox.askyesno()
-        imp = askyesno(
-            title="Important!",
-            message="Some information before you use speech to text-\n\n1. You should have an active internet connection as STT uses google speech recognition to work\n2. If you don't say anything, the program will show an error\n3. Right now you can only record for 20 seconds in one go without able to stop the recording in between\n4. Because of privacy issues, you should not say very personal/explicit things as this recording first goes to google which then extracts text from it\n5. As of now, only English language is written irrespective of the language you speak\n6. The text will be placed at the current position of your cursor in the text box, do not place cursor in the title box as it will give an error\n7. The results may not meet your expectations\n8. You will hear a sound and then you can start to speak. When 20 seconds are up, another sound will be played asking you to wait\n\nDo you wish to continue?\n",
-        )
-
-        def stt():
-            # Initialize the recognizer
-            r = Recognizer()  # speech_recognition.Recognizer()
-
-            # Sampling frequency
-            frequency = 44400
-
-            # Recording duration in seconds
-            duration = 20
-            try:
-                engine.say("Listening!")
-                engine.runAndWait()
-
-                # to record audio from
-                # sound-device into a Numpy
-                recording = rec(
-                    int(duration * frequency), samplerate=frequency, channels=2
-                )  # sounddevice.rec()
-
-                # Wait for the audio to complete
-                wait()  # sounddevice.wait()
-
-                engine.say("Processing Audio, Please wait")
-                engine.runAndWait()
-
-                # using wavio to save the recording in .wav format
-                # This will convert the NumPy array to an audio
-                # file with the given sampling frequency
-                write(
-                    f"{homepath}recording1.wav", recording, frequency, sampwidth=2
-                )  # wavio.write()
-
-                # speech_recognition.AudioFile()
-                file_audio = AudioFile(f"{homepath}recording1.wav")
-
-                with file_audio as source:
-                    audio_text = r.record(source)
-
-                rt = r.recognize_google(audio_text)
-
-                # os.remove()
-                remove(f"{homepath}recording1.wav")
-
-                text_box.insert(text_box.index(tk.INSERT), rt)
-
-                with open(f"{homepath}MyArchive/{cal.selection_get()}.txt", "w") as f:
-                    f.write(text_box.get(0.0, tk.END))
-            except Exception as e:
-                # tkinter.messagebox.showerror()
-                showerror(
-                    title="Error!",
-                    message=f"There is some error while using speech to text\n\n{e}",
-                )
-
-        if imp == True:
-            stt_threading = threading.Thread(target=stt)
-            stt_threading.start()
-        else:
-            pass
-
-    def narrator(voice):
-        if voice == "male":
-            with open(f"{homepath}Textfiles/voices.txt", "w") as f:
-                f.write("0")
-        elif voice == "female":
-            with open(f"{homepath}Textfiles/voices.txt", "w") as f:
-                f.write("1")
-        else:
-            showerror("Error!", "There is some error while changing narrator's voice")
-
     # MyArchives menu
     myarchives_menu = tk.Menu(my_menu, tearoff=False)
     my_menu.add_cascade(label="File", menu=myarchives_menu)
-    myarchives_menu.add_command(label="Speech-To-Text", command=sttt)
-    myarchives_menu.add_command(label="Text-To-Speech", command=ttst)
+    myarchives_menu.add_command(
+        label="Speech-To-Text", command=lambda: sttt(text_box, cal)
+    )
+    myarchives_menu.add_command(label="Text-To-Speech", command=lambda: ttst(text_box))
     narrator_menu = tk.Menu(my_menu, tearoff=False)
     myarchives_menu.add_cascade(label="Narrator's Voice", menu=narrator_menu)
     narrator_menu.add_command(label="Male", command=lambda: narrator("male"))
@@ -332,13 +167,19 @@ def main():
     edit_menu = tk.Menu(my_menu, tearoff=False)
     my_menu.add_cascade(label="Edit", menu=edit_menu)
     edit_menu.add_command(
-        label="Cut", command=lambda: cut_text(False), accelerator="(Ctrl+x)"
+        label="Cut",
+        command=lambda: cut_text(False, window, text_box),
+        accelerator="(Ctrl+x)",
     )
     edit_menu.add_command(
-        label="Copy", command=lambda: copy_text(False), accelerator="(Ctrl+c)"
+        label="Copy",
+        command=lambda: copy_text(False, window, text_box),
+        accelerator="(Ctrl+c)",
     )
     edit_menu.add_command(
-        label="Paste", command=lambda: paste_text(False), accelerator="(Ctrl+v)"
+        label="Paste",
+        command=lambda: paste_text(False, window, text_box),
+        accelerator="(Ctrl+v)",
     )
     edit_menu.add_separator()
     edit_menu.add_command(
@@ -349,9 +190,11 @@ def main():
     )
     edit_menu.add_separator()
     edit_menu.add_command(
-        label="Select All", command=lambda: select_all(True), accelerator="(Ctrl+a)"
+        label="Select All",
+        command=lambda: select_all(True, text_box),
+        accelerator="(Ctrl+a)",
     )
-    edit_menu.add_command(label="Clear", command=clear_all)
+    edit_menu.add_command(label="Clear", command=lambda: clear_all(text_box))
 
     # Create an Options menu item
     help_menu = tk.Menu(my_menu, tearoff=False)
---
+++ MyArchives/gui/menubar_functions.py
@@ -0,0 +1,185 @@
+# Python Libraries
+import threading
+import tkinter as tk
+from os import remove
+from tkinter.messagebox import askyesno, showerror
+
+# Third Party Libraries
+from pyttsx3 import init
+from sounddevice import rec, wait
+from speech_recognition import AudioFile, Recognizer
+from wavio import write
+
+# MyArchives Libraries
+from .home_dir import home_directory
+
+homepath = home_directory()
+
+
+# pyttsx3.init()
+engine = init()
+
+# Cut Text
+def cut_text(e, window, text_box):
+    global selected
+    # Check to see if keyboard shortcut used
+    if e:
+        selected = window.clipboard_get()
+    else:
+        if text_box.selection_get():
+            # Grab selected text from text box
+            selected = text_box.selection_get()
+
+            # Delete Selected Text from text box
+            text_box.delete("sel.first", "sel.last")
+
+            # Clear the clipboard then apptk.end
+            window.clipboard_clear()
+            window.clipboard_apptk.end(selected)
+
+
+# Copy Text
+def copy_text(e, window, text_box):
+    global selected
+    # check to see if we used keyboard shortcuts
+    if e:
+        selected = window.clipboard_get()
+
+    if text_box.selection_get():
+        # Grab selected text from text box
+        selected = text_box.selection_get()
+
+        # Clear the clipboard then apptk.end
+        window.clipboard_clear()
+        window.clipboard_apptk.end(selected)
+
+
+# Paste Text
+def paste_text(e, window, text_box):
+    global selected
+    # Check to see if keyboard shortcut used
+    if e:
+        selected = window.clipboard_get()
+    else:
+        if selected:
+            position = text_box.index(tk.INSERT)
+            text_box.insert(position, selected)
+
+
+# Select all Text
+def select_all(e, text_box):
+    # Add sel tag to select all text
+    text_box.tag_add("sel", "1.0", "tk.end")
+
+
+# Clear All Text
+def clear_all(text_box):
+    text_box.delete(1.0, tk.END)
+
+
+# Text-To-Speech (threading)
+def ttst(text_box):
+    def tts():
+        voices = engine.getProperty("voices")
+        with open(f"{homepath}Textfiles/voices.txt", "r") as f:
+            voice = f.read()
+        if voice == "0":
+            engine.setProperty(
+                "voice", voices[0].id
+            )  # changing index, changes voices. 0 for male
+            engine.say(text_box.get(0.0, tk.END))
+            engine.runAndWait()
+        elif voice == "1":
+            engine.setProperty(
+                "voice", voices[1].id
+            )  # changing index, changes voices. 1 for female
+            engine.say(text_box.get(0.0, tk.END))
+            engine.runAndWait()
+        else:
+            engine.say(
+                "There is some problem with Text To Speech, Try to change narrators voice and try again"
+            )
+            engine.runAndWait()
+
+    tts_threading = threading.Thread(target=tts)
+    tts_threading.start()
+
+
+# Speech-To-Text (threading)
+def sttt(text_box, cal):
+    # tkinter.messagebox.askyesno()
+    imp = askyesno(
+        title="Important!",
+        message="Some information before you use speech to text-\n\n1. You should have an active internet connection as STT uses google speech recognition to work\n2. If you don't say anything, the program will show an error\n3. Right now you can only record for 20 seconds in one go without able to stop the recording in between\n4. Because of privacy issues, you should not say very personal/explicit things as this recording first goes to google which then extracts text from it\n5. As of now, only English language is written irrespective of the language you speak\n6. The text will be placed at the current position of your cursor in the text box, do not place cursor in the title box as it will give an error\n7. The results may not meet your expectations\n8. You will hear a sound and then you can start to speak. When 20 seconds are up, another sound will be played asking you to wait\n\nDo you wish to continue?\n",
+    )
+
+    def stt():
+        # Initialize the recognizer
+        r = Recognizer()  # speech_recognition.Recognizer()
+
+        # Sampling frequency
+        frequency = 44400
+
+        # Recording duration in seconds
+        duration = 20
+        try:
+            engine.say("Listening!")
+            engine.runAndWait()
+
+            # to record audio from
+            # sound-device into a Numpy
+            recording = rec(
+                int(duration * frequency), samplerate=frequency, channels=2
+            )  # sounddevice.rec()
+
+            # Wait for the audio to complete
+            wait()  # sounddevice.wait()
+
+            engine.say("Processing Audio, Please wait")
+            engine.runAndWait()
+
+            # using wavio to save the recording in .wav format
+            # This will convert the NumPy array to an audio
+            # file with the given sampling frequency
+            write(
+                f"{homepath}recording1.wav", recording, frequency, sampwidth=2
+            )  # wavio.write()
+
+            # speech_recognition.AudioFile()
+            file_audio = AudioFile(f"{homepath}recording1.wav")
+
+            with file_audio as source:
+                audio_text = r.record(source)
+
+            rt = r.recognize_google(audio_text)
+
+            # os.remove()
+            remove(f"{homepath}recording1.wav")
+
+            text_box.insert(text_box.index(tk.INSERT), rt)
+
+            with open(f"{homepath}MyArchive/{cal.selection_get()}.txt", "w") as f:
+                f.write(text_box.get(0.0, tk.END))
+        except Exception as e:
+            # tkinter.messagebox.showerror()
+            showerror(
+                title="Error!",
+                message=f"There is some error while using speech to text\n\n{e}",
+            )
+
+    if imp == True:
+        stt_threading = threading.Thread(target=stt)
+        stt_threading.start()
+    else:
+        pass
+
+
+def narrator(voice):
+    if voice == "male":
+        with open(f"{homepath}Textfiles/voices.txt", "w") as f:
+            f.write("0")
+    elif voice == "female":
+        with open(f"{homepath}Textfiles/voices.txt", "w") as f:
+            f.write("1")
+    else:
+        showerror("Error!", "There is some error while changing narrator's voice")
